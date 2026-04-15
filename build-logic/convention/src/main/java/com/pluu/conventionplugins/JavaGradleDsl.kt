e826f56043c4762512fe67abc7f07e4b2b16301c
pluu <pluulove@gmail.com>
2026-04-16 01:01:26 +0900

    Cleanup code

--- build-logic/convention/src/main/java/com/pluu/conventionplugins/JavaGradleDsl.kt
+++ build-logic/convention/src/main/java/com/pluu/conventionplugins/JavaGradleDsl.kt
@@ -5,7 +5,7 @@ import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionAware
 import org.gradle.api.plugins.JavaPluginExtension
 
-internal fun Project.`java`(
+internal fun Project.java(
     configure: Action<JavaPluginExtension>
 ) {
     (this as ExtensionAware).extensions.configure("java", configure)
--- features-navigator/src/main/java/com/pluu/conventionplugins/features/common/SampleNavigator.kt
+++ features-navigator/src/main/java/com/pluu/conventionplugins/features/common/SampleNavigator.kt
@@ -1,7 +1,6 @@
 package com.pluu.conventionplugins.features.common
 
 import android.app.Activity
-import android.content.Context
 
 interface SampleNavigator {
     fun start(activity: Activity)
